b5ec2430d1417d2f04e60be2a94cb0a1c2791ec2
liaolihao <3144743728@qq.com>
2024-03-27 15:28:33 +0000

    修复了pid和qid关系计算错误的代码

--- task3.py
+++ task3.py
@@ -108,7 +108,7 @@ with open('candidate-passages-top1000.tsv', 'r', encoding='utf-8') as file:
     tsv_reader = csv.reader(file, delimiter='\t')
     for row in tsv_reader:
         qid,pid = row[0],row[1]
-        if qid not in qid_and_pid[qid]:
+        if pid not in qid_and_pid[qid]:
             qid_and_pid[qid].add(pid)
 
 # calculate the cosine_similarity
